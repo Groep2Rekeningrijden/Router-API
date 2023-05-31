725d7833979095eb2a1adf3a6c01d44cc45c8419
Fendamear <Nickvdmeer2001@gmail.com>
2023-05-31 14:30:29 +0200

    new pipeline

--- Router-Api/Router-Api/Controllers/RouteApiController.cs
+++ Router-Api/Router-Api/Controllers/RouteApiController.cs
@@ -66,7 +66,7 @@ namespace Router_Api.Controllers
             }
         }
 
-        [HttpGet("/getAllVehicleID's")]
+        [HttpGet("/getAllVehicleIDs")]
         public async Task<List<string>> GetAllVehicleIDs()
         {
             try
